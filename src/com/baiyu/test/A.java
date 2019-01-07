b1abf2bb35c7508ba5502afc147614c7c1dddc95
yu.bai <yu.bai@weimob.com>
2019-01-07 19:20:26 +0800

    类加载

---
+++ src/com/baiyu/test/A.java
@@ -0,0 +1,39 @@
+package com.baiyu.test;
+
+/**
+ * @author baiyu
+ * @description: A
+ * @date: 2019/1/4
+ */
+public class A {
+
+    static class Father{
+        public static String ff = "6";
+        static {
+            System.out.println(5);
+        }
+        {
+            System.out.println(6);
+        }
+    }
+
+    static class Son extends Father{
+        static {
+            System.out.println(3);
+        }
+        {
+            System.out.println(4);
+        }
+    }
+
+    static {
+        System.out.println(1);
+    }
+    {
+        System.out.println(2);
+    }
+
+    public static void main(String[] args) {
+        String m = Son.ff;
+    }
+}
---
+++ src/com/baiyu/test/B.java
@@ -0,0 +1,50 @@
+package com.baiyu.test;
+
+import java.nio.channels.Pipe;
+
+/**
+ * @author baiyu
+ * @description: B
+ * @date: 2019/1/7
+ */
+public class B {
+   static class F{
+       static final String f = "f";
+       static {
+           System.out.println("1");
+       }
+       {
+           System.out.println("2");
+       }
+       static int m(){
+           System.out.println(7);
+           return 8;
+       }
+   }
+
+    static class S1 extends F{
+        static String f2 = "s1";
+        static {
+            System.out.println("3");
+        }
+        {
+            System.out.println("4");
+        }
+    }
+
+    static class S2 extends F{
+        static {
+            System.out.println("5");
+        }
+        {
+            System.out.println("6");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(S2.f);
+        System.out.println(S1.f2);
+        System.out.println(S2.m());
+
+    }
+}
